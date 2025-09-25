b06fe0794107289473e3ad99d7ef7490fa637544
sathvikpd7 <sathvikpd2005@gmail.com>
2025-09-25 21:49:51 +0530

    Delete all files

--- bot/sdc-registration-app/frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center py-4">
-      <p>&copy; {new Date().getFullYear()} SDC. All rights reserved.</p>
-    </footer>
-  );
-}
